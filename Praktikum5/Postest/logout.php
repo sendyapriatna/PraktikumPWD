1d56c643713f085cb273ae7bc8849aeb853d4377
sendyapriatna <sendyapriatna001@gmail.com>
2021-11-05 20:26:07 +0700

    Praktikum-Postest 5

---
+++ Praktikum5/Postest/logout.php
@@ -0,0 +1,8 @@
+<?php
+
+// Session start digunakan untuk memulai session 
+session_start();
+
+// Session destroy digunakan untuk menghapus semua session yang ada
+session_destroy();
+echo "Anda telah sukses keluar sistem <b>LOGOUT</b>";
